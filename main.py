8bd781261ce54433a59b70b42075c9068f41fb50
quyet.lm <quyet.lm@teko.vn>
2023-11-30 02:20:54 +0700

    lint

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def hello():
     return {"hello": "world"}
 
 @app.post("/day/advice")
-async def day_advice(input: DayAdvice):
+async def day_advice(req: DayAdvice):
     stream = llm("""<|im_start|>system
 You are a helpful chatbot and a doctor with 20 years of experience and a scientist about air quality. You will be provided information about the air quality of a place, including 
 the AQI, NO2, O3, SO2, PM2.5, PM10. From that information, you will give an advice for a person to stay healthy in that day in that place. The advice should be 2 to 3 sentences.
@@ -56,27 +56,27 @@ The response should be in json format. The advice is in a string called "advice"
 <|im_end|>
 <|im_start|>user
 The air quality metrics are as the following: AQI={}, NO2={}, O3={}, SO2={}, PM2.5={}, PM10={}<|im_end|>
-<|im_start|>assistant""".format(input.aqi, input.no2, input.o3, input.so2, input.pm2_5, input.pm10), max_tokens=500,  stop=["<|im_end|>"], stream=False)
+<|im_start|>assistant""".format(req.aqi, req.no2, req.o3, req.so2, req.pm2_5, req.pm10), max_tokens=500,  stop=["<|im_end|>"], stream=False)
     result = copy.deepcopy(stream)
     raw = clean(result['choices'][0]['text'])
     return json.loads(raw)
 
 @app.post("/predict/disease")
-async def predict_disease(input: Disease):
+async def predict_disease(req: Disease):
     template = """<|im_start|>system
 You are a helpful chatbot and a doctor with 20 years of experience. You will be provided a paragraph describing health issues of a person. From that paragraph, extract the list of diseases that person has. 
 The response should be in json format. The disease(s) should be in a list called "diseases". 
 <|im_end|>
 <|im_start|>user
 Here is the paragraph to extract information from: {}<|im_end|>
-<|im_start|>assistant""".format(input.description)
+<|im_start|>assistant""".format(req.description)
     stream = llm(template, max_tokens=500,  stop=["<|im_end|>"], stream=False)
     result = copy.deepcopy(stream)
     raw = clean(result['choices'][0]['text'])
     return json.loads(raw)
 
 @app.post("/personal/advice")
-async def personal_advice(input: PersonalAdvice):
+async def personal_advice(req: PersonalAdvice):
     stream = llm("""<|im_start|>system
 You are a helpful chatbot and a doctor with 20 years of experience and a scientist about air quality. You will be provided information about the air quality of a place, including 
 the AQI, NO2, O3, SO2, PM2.5, PM10; and a description about health issues of a person. From that information, you will give a list of 3 advices for that person to stay healthy in that day in that place. The advice should be short in 1 sentence. 
@@ -84,7 +84,7 @@ The response should be in json format. The advices should be in a list called "a
 <|im_end|>
 <|im_start|>user
 The air quality metrics are as the following: AQI={}, NO2={}, O3={}, SO2={}, PM2.5={}, PM10={}. The health issues are: {}<|im_end|>
-<|im_start|>assistant""".format(input.aqi, input.no2, input.o3, input.so2, input.pm2_5, input.pm10, input.description), max_tokens=500,  stop=["<|im_end|>"], stream=False)
+<|im_start|>assistant""".format(req.aqi, req.no2, req.o3, req.so2, req.pm2_5, req.pm10, req.description), max_tokens=500,  stop=["<|im_end|>"], stream=False)
     result = copy.deepcopy(stream)
     raw = clean(result['choices'][0]['text'])
     return json.loads(raw)
