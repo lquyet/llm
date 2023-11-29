619aee8e889d5ef949892c7a73d0a9560e9fe34f
quyet.lm <quyet.lm@teko.vn>
2023-11-30 02:14:54 +0700

    del unused

--- run.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False, log_level="debug", debug=True,
-                workers=1, limit_concurrency=1, limit_max_requests=1)
